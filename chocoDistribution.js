6b5d797f7bba1e733001bd95df2cc16919be4152
Ravi Soni <surajsoni020997@gmail.com>
2024-02-27 00:50:27 +0530

    Chocolate Distribution Problem

---
+++ chocoDistribution.js
@@ -0,0 +1,7 @@
+// Given a certain number of chocolates and have to distribute equally between the children then how many chocolates will remain in the end:
+
+var chocolates = 8;
+var child = 5;
+var rem = chocolates % child;
+
+console.log(" Total remaining chocolates are " + rem)
