303ff8a2c7547f8b04edafb5f41acd250ea01e38
Ravi Soni <surajsoni020997@gmail.com>
2024-02-27 17:22:25 +0530

    Conditional Statement

---
+++ condtnStatement.js
@@ -0,0 +1,30 @@
+// Starting from the if statement:
+
+var age = 29;
+if (age > 25) {
+    console.log(" Age is greater than 25 or eqaul ");
+}
+
+// Now comes to the if-else statement:
+
+var marks = 78;
+if (marks < 60) {
+    console.log("Student is Failed");
+}else{
+    console.log("Student is Passed");
+}
+
+// Here is the last but not least else-if statement:
+
+var total_marks = 50;
+var obtained_marks = 35;
+
+if (obtained_marks === total_marks) {
+    console.log("Passed with 100% marks");
+}
+else if (obtained_marks < total_marks){
+    console.log("Not Passed with 100% marks");
+}
+else{
+    console.log("Not Found Your result")
+}
