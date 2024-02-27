c380ef643608d92e97586e7f100cca59af147d99
Ravi Soni <surajsoni020997@gmail.com>
2024-02-27 15:43:45 +0530

    Double and Triple not eqaul Operator

--- eqlOperator.js
+++ eqlOperator.js
@@ -6,5 +6,11 @@ var num2 = "3";
 console.log(num1 == num2);
 
 //Now triple eqaul operator is stating the big difference while considering it as a datatype:
-
 console.log(num1 === num2);
+
+// We have not equal to opeartor also which work as same sa it, here output wil be false:
+console.log(num1 != num2);
+
+// Considering the !== now:
+console.log(num1 !== num2);
+
