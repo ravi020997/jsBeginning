675e96366c2348aed0a72b2b0a5d2f7c2302550f
Ravi Soni <surajsoni020997@gmail.com>
2024-02-27 15:35:52 +0530

    Double and Triple equal Operator

---
+++ eqlOperator.js
@@ -0,0 +1,10 @@
+//Stating the difference b/w double and triple equal operator:
+
+var num1 = 3;
+var num2 = "3";
+//So here it is printing true because value is same, it doesn't care about the datatypes:
+console.log(num1 == num2);
+
+//Now triple eqaul operator is stating the big difference while considering it as a datatype:
+
+console.log(num1 === num2);
