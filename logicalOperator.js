e38d83fd5f4014c4473a990cefa167de5f80e345
Ravi Soni <surajsoni020997@gmail.com>
2024-02-27 16:12:53 +0530

    Logcal Operator Working

---
+++ logicalOperator.js
@@ -0,0 +1,17 @@
+// Here is the example of AND(&&) operator where both the side must be true or false:
+
+var x = 3;
+var y = 5;
+
+console.log((x !=5) && (y > x));
+console.log((x == y) && (x != y));
+
+// Now next operator is OR(||) where both side must be differ:
+
+console.log((x > y) || (y > x));
+console.log((x < y) || (y > x));
+
+// Here is ! opeartor which turns thr value in oppostte:
+
+console.log(!x < y);
+console.log(!x > y);
