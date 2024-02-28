eb9ba151b8119bd23e4907770a69eb4743b7c6d5
Ravi Soni <surajsoni020997@gmail.com>
2024-02-28 23:53:24 +0530

    Gender&Age Probelm

---
+++ codeInBlock.js
@@ -0,0 +1,18 @@
+var age = 35;
+var gender = "Male";
+
+if (age > 30) {
+    if (gender == "Male") {
+        console.log("Male > 30");
+    }
+    else{
+        console.log("Male < 30")
+    }
+    if(age < 30){
+        elseif (gender == "Female")
+            console.log("Female > 30");
+        }
+        else{
+        console.log("Female < 30");
+    }
+}
